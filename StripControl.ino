a1ac6f53274c9b99d29c09826c263f38b144d8fc
Lukas <lukas.freeze@gmail.com>
2018-07-24 16:58:20 +0200

    Update StripControl.ino
    functions for individual strips and both sides at once; tested various parameters

--- StripControl.ino
+++ StripControl.ino
@@ -1,8 +1,6 @@
 /*
   StripControl
-
   Turns four LED strips to the selected value (255=100%). Values will vary depending on ambient light!
-
   2018-7-20 by Lukas Frese
 */
 int front = 11;
@@ -22,7 +20,20 @@ void setup() {
 }
 
 void loop() {
-  all(55);
+  // +++ 8 L +++
+  //all(155);
+  //leftOn(125); 
+  //rightOn(125);
+  //sides(155); //(covered in front) 255: too high -- 55: too low -- 125 still too low(?) -- 200: can disappear near top
+  
+  //sides(170); //(uncovered)
+  //frontOn(20); //should be much lower than back, maybe off completely
+  //backOn(155);
+
+  // +++ 5 L +++
+  sides(200); //170 a bit low? -- 50 too low, 250 too much
+  frontOn(20); //should be much lower than back, maybe off completely
+  backOn(155);
 }
 
 //all four strips at the same value - probably not usable
@@ -33,3 +44,23 @@ void all(int brightness){
   analogWrite(right, brightness);   // controls the right LED strip
 }
 
+void sides(int brightness){
+  analogWrite(left, brightness);   // controls the left LED strip
+  analogWrite(right, brightness);   // controls the right LED strip
+}
+
+void leftOn(int brightness){
+  analogWrite(left, brightness);   // controls the left LED strip
+}
+
+void rightOn(int brightness){
+  analogWrite(right, brightness);   // controls the right LED strip
+}
+
+void frontOn(int brightness){
+  analogWrite(front, brightness);   // controls the front LED strip
+}
+
+void backOn(int brightness){
+  analogWrite(back, brightness);   // controls the back LED strip
+}
